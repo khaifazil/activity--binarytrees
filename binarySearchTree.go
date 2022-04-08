81f5529675828d9d1a7eeb6ac7fec9117006f300
khaifazil <khairal.fazil@gmail.com>
2022-04-08 11:06:43 +0800

    added removal of nodes

--- binarySearchTree.go
+++ binarySearchTree.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type binaryNode struct {
 	item  string
@@ -66,13 +69,43 @@ func (bst *BST) searchNode(t *binaryNode, item string) *binaryNode {
 		} else {
 			if t.item > item {
 				return bst.searchNode(t.left, item)
-			}else{
+			} else {
 				return bst.searchNode(t.right, item)
 			}
 		}
 	}
 }
 
+func (bst *BST) findSuccessor(t *binaryNode) string{
+	for t.right != nil { //find node on extreme right (might have left hand sub tree)
+		t = t.right
+	}
+	return t.item
+}
+
+func (bst *BST) removeNode(t **binaryNode, item string) (*binaryNode, error){
+	if *t == nil { //1st case
+		return nil, errors.New("Tree is empty")
+	}else if item < (*t).item {
+		(*t).left, _ = bst.removeNode(&(*t).left, item)
+	}else if item > (*t).item {
+		(*t).right, _ = bst.removeNode(&(*t).right, item)
+	}else { //2nd case
+		if (*t).left == nil {
+			return (*t).right, nil
+		} else if (*t).right == nil {
+			return (*t).left, nil
+		}else { //3rd case of 2 children
+			(*t).item = bst.findSuccessor((*t).left)
+			(*t).left, _ = bst.removeNode(&(*t).left, item)
+		}
+	}
+	return *t, nil
+}
+
+func (bst *BST) remove(item string) {
+	bst.root, _ = bst.removeNode(&bst.root, item)
+}
 
 //utility functions
 func (bst *BST) inOrder() {
@@ -93,17 +126,22 @@ func main() {
 	bst.insert("Tom")
 	bst.insert("Lucas")
 
-	fmt.Println("inorder")
+	fmt.Println("Inorder")
 	bst.inOrder()
-	fmt.Println("preorder")
-	bst.preOrder()
+	// fmt.Println("preorder")
+	// bst.preOrder()
 
-	var name string = "John"
-	fmt.Println("Searching for item...")
-	t := bst.search(name)
-	if t != nil {
-		fmt.Println(name, "is present")
-	}else {
-		fmt.Println(name, "is not present")
-	}
+	// var name string = "John"
+	// fmt.Println("Searching for item...")
+	// t := bst.search(name)
+	// if t != nil {
+	// 	fmt.Println(name, "is present")
+	// } else {
+	// 	fmt.Println(name, "is not present")
+	// }
+
+	fmt.Println("Removing some node...")
+	bst.remove("Mary")
+	fmt.Println("Inorder")
+	bst.inOrder()
 }
